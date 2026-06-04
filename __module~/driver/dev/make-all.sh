033ba3ba91003f047fa0d0a9fac234c7fe1c2056
pengcheng <peng.cheng@acyclic.ai>
2026-06-04 12:50:27 -0400

    copy build result to Plugins dir

--- __module~/driver/dev/make-all.sh
+++ __module~/driver/dev/make-all.sh
@@ -10,3 +10,9 @@ cargo zigbuild --target aarch64-apple-darwin --release
 # TODO: this will fail on OS without libudev (I mean linux)
 cargo zigbuild --target x86_64-unknown-linux-gnu --release || \
   cargo zigbuild --target x86_64-unknown-linux-gnu --release --no-default-features --features rokid,nreal,grawoow
+
+echo "[Copying to Plugins ...]"
+cp target/x86_64-pc-windows-gnu/release/ar_drivers.dll ../../Plugins/
+cp target/aarch64-apple-darwin/release/libar_drivers.dylib ../../Plugins/
+cp target/x86_64-unknown-linux-gnu/release/libar_drivers.so ../../Plugins/
+
