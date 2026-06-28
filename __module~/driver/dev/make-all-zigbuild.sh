38634e879b651029537dfd6868c65e1ac646c1fa
Peng Cheng <peng.cheng@acyclic.ai>
2026-06-27 21:47:45 -0400

    add a TODO for a zigbuild bug

--- __module~/driver/dev/make-all-zigbuild.sh
+++ __module~/driver/dev/make-all-zigbuild.sh
@@ -6,7 +6,9 @@ cargo clean
 
 echo "[Start Compiling ... Zig compiler and cargo-zigbuild must be installed]"
 
+# TODO: disable until https://github.com/rust-cross/cargo-zigbuild/issues/251 is fixed
 cargo zigbuild --target x86_64-pc-windows-gnu --release || echo "WARNING: Failed to build for x86_64-pc-windows-gnu"
+
 cargo zigbuild --target aarch64-apple-darwin --release || echo "WARNING: Failed to build for aarch64-apple-darwin"
 
 # TODO: this will fail on OS without libudev (I mean linux)
