9dcc0c9f2b1b018f6648e64c903e574b6659065c
Peng Cheng <peng.cheng@acyclic.ai>
2026-08-13 17:25:31 -0400

    feed estimated gravity direction in update_mag

--- __module~/driver/src/fusion/naive_cf.rs
+++ __module~/driver/src/fusion/naive_cf.rs
@@ -137,13 +137,12 @@ impl NaiveCF {
     pub(super) fn update_mag(&mut self, mag_rub: &Vector3<f32>, t: u64) -> () {
         let raw_mag = rub_to_frd(mag_rub); // reading is always muT (microTesla) pointing to north
 
-        // TODO: add gravity direction
-        //  this usage of evaluate_correct assumes no available gravity direction
-        //  not true, gravity direction is already estimated from other sensors
+        // gravity direction is already estimated by the acc complementary filter
+        let gravity_frd = self.state.attitude.inverse() * Self::UP_FRD;
         let mag_north: Vector3<f32> = match self
             .state
             .mag
-            .evaluate_correct(raw_mag, None, t)
+            .evaluate_correct(raw_mag, Some(gravity_frd), t)
             .ok()
             .and_then(|result| result.calibrated())
         {
